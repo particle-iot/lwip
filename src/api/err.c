453e2102f432e13aab57b2329b63355a67917832
Andrey Tolstoy <avtolstoy@gmail.com>
2026-07-07 01:10:26 +0700

    [api] err: map ERR_IF to EIO instead of -1

--- src/api/err.c
+++ src/api/err.c
@@ -58,7 +58,7 @@ static const int err_to_errno_table[] = {
   EALREADY,      /* ERR_ALREADY    -9      Already connecting.      */
   EISCONN,       /* ERR_ISCONN     -10     Conn already established.*/
   ENOTCONN,      /* ERR_CONN       -11     Not connected.           */
-  -1,            /* ERR_IF         -12     Low-level netif error    */
+  EIO,           /* ERR_IF         -12     Low-level netif error    */
   ECONNABORTED,  /* ERR_ABRT       -13     Connection aborted.      */
   ECONNRESET,    /* ERR_RST        -14     Connection reset.        */
   ENOTCONN,      /* ERR_CLSD       -15     Connection closed.       */
